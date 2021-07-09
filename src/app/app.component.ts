7c4f2ee91748b90d2ad6476aeb9d547a671a7091
Shaklin Syed <theviralgriffin@Shaklins-MacBook-Air.local>
2021-07-09 22:37:45 +0530

    #0 Testing Collobration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+
+import Quill from 'quill';
+import QuillCursors from "quill-cursors";
+import { WebsocketProvider } from 'y-websocket'
+
+import * as Y from 'yjs'
+import { QuillBinding } from 'y-quill'
+
+var document: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'evernote-clone';
+
+export class AppComponent implements AfterViewInit {
+
+  quill: any;
+  ydoc: any;
+  ytext: any;
+  binding: any;
+
+  @ViewChild("editor",{static: false}) editor: any;
+  
+  ngAfterViewInit() {
+
+    Quill.register('modules/cursors', QuillCursors);
+    this.quill = new Quill(this.editor.nativeElement, {
+      modules: {
+        cursors: true,
+        toolbar: [
+          // adding some basic Quill content features
+          [{ header: [1, 2, false] }],
+          ['bold', 'italic', 'underline'],
+          ['image', 'code-block']
+        ],
+        history: {
+          // Local undo shouldn't undo changes
+          // from remote users
+          userOnly: true
+        }
+      },
+      placeholder: 'Start collaborating...',
+      theme: 'snow' // 'bubble' is also great
+    })
+  
+    // // A Yjs document holds the shared data
+    // this.ydoc = new Y.Doc();
+  
+    // // Define a shared text type on the document
+    // this.ytext = this.ydoc.getText('quill');
+  
+    // // Create an editor-binding which
+    // // "binds" the quill editor to a Y.Text type. 
+    // this.binding = new QuillBinding(this.ytext, this.quill);
+
+    // const provider = new WebsocketProvider(
+    //   'wss://demos.yjs.dev', 'quill-demo-room', this.ydoc
+    // )
+
+    // this.quill.on('text-change', function(delta: any, oldDelta: any, source: any) {
+
+    //   console.log(delta);
+    //   console.log(oldDelta);
+    //   console.log(source);
+    //   if (source == 'api') {
+    //     console.log("An API call triggered this change.");
+    //   } else if (source == 'user') {
+    //     console.log("A user action triggered this change.");
+    //   }
+    // });
+    
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
-import { QuillModule } from 'ngx-quill'
+// import { QuillModule } from 'ngx-quill'
 
 @NgModule({
   declarations: [
@@ -12,8 +14,18 @@ import { QuillModule } from 'ngx-quill'
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
-    QuillModule.forRoot()
+    // QuillModule.forRoot({
+    //   modules: {
+    //     cursors: true,
+    //     history: {
+    //       // Local undo shouldn't undo changes
+    //       // from remote users
+    //       userOnly: true
+    //     }
+    //   }
+    // })
   ],
   providers: [],
   bootstrap: [AppComponent]
